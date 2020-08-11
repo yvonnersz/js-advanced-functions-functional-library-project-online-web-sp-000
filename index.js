c046a01d0aa15dce288301271e3e5368748c86f9
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 09:35:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fi = (function() {
 
     each: function(collection, callback) {
       for (let i=0; i < collection.length; i++) {
-        callback(element, index, collection)
+        callback(collection[i], i, collection)
       }
       return collection
     },
