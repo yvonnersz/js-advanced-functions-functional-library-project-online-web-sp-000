39c3e059ca043396c4132a8fb39e97edc925a6b8
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 08:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      console.log(collection)
+      console.log(callback)
     },
 
     map: function() {
